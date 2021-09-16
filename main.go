bd63ec6d7b692f14cfeba4069ff46122486a0b47
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-16 20:18:47 +0700

    Koneksi Database

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/pustaka-api?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Database connection succeed")
+
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
